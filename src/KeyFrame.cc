a5c3dc1b251e4c7318b9d45ebd640e1087792196
Giuseppe Mollica <giuseppemoll08@gmail.com>
2022-06-24 15:09:06 +0200

    MOD: updated BoW searching from 4 to 0 levels

--- src/KeyFrame.cc
+++ src/KeyFrame.cc
@@ -102,7 +102,9 @@ void KeyFrame::ComputeBoW()
         vector<cv::Mat> vCurrentDesc = Converter::toDescriptorVector(mDescriptors);
         // Feature vector associate features with nodes in the 4th level (from leaves up)
         // We assume the vocabulary tree has 6 levels, change the 4 otherwise
-        mpORBvocabulary->transform(vCurrentDesc,mBowVec,mFeatVec,4);
+        
+        // mpORBvocabulary->transform(vCurrentDesc,mBowVec,mFeatVec,4);
+        mpORBvocabulary->transform(vCurrentDesc,mBowVec,mFeatVec,0);
         printf("%s Extracted %d BOW FEATURES \n", __PRETTY_FUNCTION__, mBowVec.size());
     }
 }
